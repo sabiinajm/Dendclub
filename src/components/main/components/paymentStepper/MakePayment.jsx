27b3060865acc3e7742118077ab837a8a7d2efc3
Soltan <mahmudovsoltan477@gmail.com>
2025-06-26 23:42:48 +0400

    finish queue page

--- src/components/main/components/paymentStepper/MakePayment.jsx
+++ src/components/main/components/paymentStepper/MakePayment.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const formatExpiryDate = (value) => {
-    const digits = value.replace(/\D/g, "");
+  const digits = value.replace(/\D/g, "");
 
   // 2) Heç nə yoxdursa, boş qaytar
   if (digits.length === 0) return "";
@@ -46,7 +46,7 @@ const MakePayment = () => {
             placeholder="Adınızı yazın"
           />
         </div>
-        <div className="flex flex-col gap-[6px]" >
+        <div className="flex flex-col gap-[6px]">
           <label htmlFor="" className="text-sm text-[#2C2C2E] font-medium ">
             İstifadə müddəti
           </label>
@@ -58,26 +58,78 @@ const MakePayment = () => {
             onChange={handleChange2}
             maxLength={7}
             inputMode="numeric"
-            style={{
-              width: "100px",
-              padding: "8px",
-              fontSize: "16px",
-              borderRadius: "8px",
-              border: "1px solid #ccc",
-            }}
           />
         </div>
       </div>
-      <div>
-        <label>Kart Nömrəsi</label>
-        <input
-          type="text"
-          maxLength={19} // 16 rəqəm + 3 boşluq
-          value={cardNumber}
-          onChange={handleChange}
-          placeholder="1234 5678 9012 3456"
-          inputMode="numeric" // Mobil cihazlarda rəqəm klaviaturası açılır
-        />
+      <div className="flex  gap-4 items-center mt-5">
+        <div className="flex flex-col gap-[6px] ">
+          <label>Kart Nömrəsi</label>
+          <div className="w-[304px] border border-[#D1D1D6] flex items-center gap-2 rounded-xl py-[10px] px-[14px]">
+            <svg
+              width="34"
+              height="24"
+              viewBox="0 0 34 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                x="0.5"
+                y="0.5"
+                width="33"
+                height="23"
+                rx="3.5"
+                fill="white"
+              />
+              <rect
+                x="0.5"
+                y="0.5"
+                width="33"
+                height="23"
+                rx="3.5"
+                stroke="#F5F5F5"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M17.179 16.8288C15.9949 17.8268 14.459 18.4293 12.7807 18.4293C9.03582 18.4293 6 15.4296 6 11.7293C6 8.02899 9.03582 5.0293 12.7807 5.0293C14.459 5.0293 15.9949 5.63179 17.179 6.62984C18.363 5.63179 19.8989 5.0293 21.5773 5.0293C25.3221 5.0293 28.358 8.02899 28.358 11.7293C28.358 15.4296 25.3221 18.4293 21.5773 18.4293C19.8989 18.4293 18.363 17.8268 17.179 16.8288Z"
+                fill="#ED0006"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M17.179 16.8288C18.6369 15.5999 19.5614 13.7713 19.5614 11.7293C19.5614 9.68734 18.6369 7.85874 17.179 6.62984C18.363 5.63179 19.8989 5.0293 21.5772 5.0293C25.3221 5.0293 28.3579 8.02899 28.3579 11.7293C28.3579 15.4296 25.3221 18.4293 21.5772 18.4293C19.8989 18.4293 18.363 17.8268 17.179 16.8288Z"
+                fill="#F9A000"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M17.179 16.8278C18.6369 15.5989 19.5614 13.7703 19.5614 11.7283C19.5614 9.6864 18.6369 7.85781 17.179 6.62891C15.7211 7.85781 14.7966 9.6864 14.7966 11.7283C14.7966 13.7703 15.7211 15.5989 17.179 16.8278Z"
+                fill="#FF5E00"
+              />
+            </svg>
+            <input
+              className=" w-full  outline-none "
+              type="text"
+              maxLength={19} // 16 rəqəm + 3 boşluq
+              value={cardNumber}
+              onChange={handleChange}
+              placeholder="1234 5678 9012 3456"
+              inputMode="numeric" // Mobil cihazlarda rəqəm klaviaturası açılır
+            />
+          </div>
+        </div>
+        <div className="flex flex-col gap-[6px]">
+          <label htmlFor="" className="text-sm text-[#2C2C2E] font-medium ">
+            CVV
+          </label>
+          <input
+            className="w-[112px] border border-[#D1D1D6] py-[10px] px-[14px] rounded-xl outline-none  "
+            type="password"
+            placeholder="***"
+            maxLength={3}
+            inputMode="numeric"
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/main/components/stepperHome/StepperHeader.jsx
+++ src/components/main/components/stepperHome/StepperHeader.jsx
@@ -1,4 +1,4 @@
-const stepgeader = [
+const stepHeader = [
   {
     title: "Sahəni seç",
     line: true,
@@ -16,63 +16,142 @@ const stepgeader = [
     line: false,
   },
 ];
-const StepperHeader = ({ count }) => {
+const stepHeader2 = [
+  {
+    title: "Görüş zamanını seçin",
+  },
+  {
+    title: "Ödənişi edin",
+    latest: "Son",
+  },
+];
+const StepperHeader = ({ count, varinat, count2 }) => {
+  console.log(count2);
+
   return (
-    <div className="flex items-center mt-[51px] mb-[111px] ">
-      {stepgeader.map((step, index) => (
-        <div key={index} className="flex items-center  justify-center">
-          <div className="flex items-center justify-center flex-col w-[203px] gap-[18px] h-[76px] ">
+    <>
+      {varinat === 1 ? (
+        <div className="flex items-center  mb-[111px] ">
+          {stepHeader.map((step, index) => (
+            <div key={index} className="flex items-center  justify-center">
+              <div className="flex items-center justify-center flex-col w-[203px] gap-[18px] h-[76px] ">
+                <div
+                  className={`w-[40px] h-[40px] rounded-full border flex items-center justify-center  text-sm text-[#AEAEB2] ${
+                    count === index
+                      ? "border-[#30D158] "
+                      : count > index
+                      ? " bg-[#30D158]"
+                      : "border-[#AEAEB2] "
+                  }`}
+                >
+                  <p
+                    className={`w-4 h-4  flex items-center rounded-full justify-center ${
+                      count === index
+                        ? "bg-[#30D158] text-[#30D158]"
+                        : "text-[#AEAEB2]"
+                    }`}
+                  >
+                    {count === index ? (
+                      ""
+                    ) : count > index ? (
+                      <svg
+                        width="17"
+                        height="12"
+                        viewBox="0 0 17 12"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M0.875 6L5.875 11L15.875 1"
+                          stroke="white"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                    ) : (
+                      index + 1
+                    )}
+                  </p>
+                </div>
+                <p className="text-[#2C2C2E] text-sm font-medium ">
+                  {step.title}
+                </p>
+              </div>
+              {step.line && index < stepHeader.length - 1 && (
+                <div
+                  className={`w-[56px] h-[2px] bg-[#AEAEB2] ${
+                    count > index ? "bg-[#30D158]" : "bg-[#AEAEB2]"
+                  }`}
+                ></div>
+              )}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center gap-[291px] mb-[74px]">
+          {stepHeader2.map((stephead, i) => (
             <div
-              className={`w-[40px] h-[40px] rounded-full border flex items-center justify-center  text-sm text-[#AEAEB2] ${
-                count === index
-                  ? "border-[#30D158] "
-                  : count > index
-                  ? " bg-[#30D158]"
-                  : "border-[#AEAEB2] "
-              }`}
+              key={i}
+              className="flex flex-col items-center justify-center gap-3"
             >
-              <p
-                className={`w-4 h-4  flex items-center rounded-full justify-center ${
-                  count === index
-                    ? "bg-[#30D158] text-[#30D158]"
-                    : "text-[#AEAEB2]"
+              <div
+                className={`w-[40px] h-[40px] rounded-full border  flex items-center justify-center ${
+                  count2 === i + 1
+                    ? "border-[#30D158] "
+                    : count2 > i + 1
+                    ? " bg-[#30D158]"
+                    : "border-[#AEAEB2] "
                 }`}
               >
-                {count === index ? (
-                  ""
-                ) : count > index ? (
-                  <svg
-                    width="17"
-                    height="12"
-                    viewBox="0 0 17 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+                {stephead.latest ? (
+                  <span
+                    className={`text-[#AEAEB2] text-sm  ${
+                      count2 === i + 1
+                        ? "bg-[#30D158] w-4 h-4 rounded-full "
+                        : count2 > i + 1
+                        ? " bg-[#30D158]"
+                        : "border-[#AEAEB2] "
+                    } `}
                   >
-                    <path
-                      d="M0.875 6L5.875 11L15.875 1"
-                      stroke="white"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
+                    {count2 === i + 1 ? null : stephead.latest}
+                  </span>
                 ) : (
-                  index + 1
+                  <span
+                    className={`w-4 h-4 rounded-full bg-[#30D158]  ${
+                      count2 === i + 1
+                        ? "border-[#30D158] "
+                        : count2 > i + 1
+                        ? " bg-[#30D158]"
+                        : "border-[#AEAEB2] "
+                    }`}
+                  >
+                    {count2 > i + 1 && (
+                      <svg
+                        width="17"
+                        height="12"
+                        viewBox="0 0 17 12"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M0.875 6L5.875 11L15.875 1"
+                          stroke="white"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                    )}
+                  </span>
                 )}
-              </p>
+              </div>
+              <p>{stephead.title}</p>
             </div>
-            <p className="text-[#2C2C2E] text-sm font-medium ">{step.title}</p>
-          </div>
-          {step.line && index < stepgeader.length - 1 && (
-            <div
-              className={`w-[56px] h-[2px] bg-[#AEAEB2] ${
-                count > index ? "bg-[#30D158]" : "bg-[#AEAEB2]"
-              }`}
-            ></div>
-          )}
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   );
 };
 
